f3b2d8d3a6961926a5934582b550f60ec378317e
songmho <songmho@gmail.com>
2015-09-26 22:12:43 +0900

    파스 연결

---
+++ app/src/main/java/com/asc/coy/Coy.java
@@ -0,0 +1,23 @@
+package com.asc.coy;
+
+import android.app.Application;
+
+import com.parse.Parse;
+import com.parse.ParseACL;
+
+/**
+ * Created by songmho on 2015-09-26.
+ */
+public class Coy extends Application{
+        String APPLICATION_ID="kIXQWqLpWBsdKAkMZTfY7Z50ecL5UsLLXabHQmn9";
+        String CLIENT_KEY="vWtnUeBVZYCR6ZR2EiO4jWUBDLKAvNkKPu5qDe5f";
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Parse.initialize(this, APPLICATION_ID, CLIENT_KEY);
+        ParseACL defaultACL=new ParseACL();
+        defaultACL.setPublicReadAccess(true);
+        defaultACL.setPublicWriteAccess(true);
+        ParseACL.setDefaultACL(defaultACL,true);
+    }
+}
