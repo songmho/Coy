e7018dbdd3cc071e9f78de96ede2d69988b9ea46
songmho <songmho@gmail.com>
2015-11-10 00:32:41 +0900

    로그인 액티비티 추가

---
+++ app/src/main/java/com/asc/coy/Login_Activity.java
@@ -0,0 +1,16 @@
+package com.asc.coy;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by songmho on 15. 11. 10..
+ */
+public class Login_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
