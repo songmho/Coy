cb3e5fa4d22db7c9120eb8938a66637974b4cd4d
songmho <songmho@gmail.com>
2015-11-19 11:00:58 +0900

    setupActivity에서 현재 버전출력 및 로그아웃버튼 생성. 로그아웃 후 로그인하고 사용할 수 있는 부분 처리 필요

--- app/src/main/java/com/asc/coy/Setup_Activity.java
+++ app/src/main/java/com/asc/coy/Setup_Activity.java
@@ -1,8 +1,17 @@
 package com.asc.coy;
 
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.parse.LogOutCallback;
+import com.parse.ParseException;
+import com.parse.ParseUser;
 
 /**
  * Created by songmho on 15. 11. 17..
@@ -17,5 +26,26 @@ public class Setup_Activity extends AppCompatActivity {
         setSupportActionBar(toolbar);
         getSupportActionBar().setTitle("설정");
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        TextView version=(TextView)findViewById(R.id.version);
+        Button logout=(Button)findViewById(R.id.logout);
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ParseUser.logOutInBackground(new LogOutCallback() {
+                    @Override
+                    public void done(ParseException e) {
+                        Toast.makeText(Setup_Activity.this, "로그아웃되었습니다.", Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+        });
+
+
+        try {
+            version.setText(getPackageManager().getPackageInfo(getPackageName(), 0).versionName);
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 }
